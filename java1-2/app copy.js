757747a59320f78634866a04b029124dc4128670
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-06-17 04:49:01 +0000

    let 変数

---
+++ java1-2/app copy.js
@@ -0,0 +1,18 @@
+// 変数の書き方
+// 整数の代入
+
+let int1 = 1;
+// integer(整数)
+
+let int2 = -10;
+// 負数を代入
+
+let float1 = 3.14;
+// 小数点
+
+let str1 = "JavaScriptを覚えよう";
+// string(文字型)
+
+// この後に alert(変数); を追記すれば、アラートウィンドウ上に表示
+
+alert(str1);
---
+++ java1-2/app.js
@@ -0,0 +1,5 @@
+// 変数の書き方
+// 変数名の前にletを付けることで、変数を宣言（rubyのputs）
+
+let hello = "Hello, World";
+alert(hello)
